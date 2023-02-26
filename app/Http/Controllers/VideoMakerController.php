e41366bf6b9fcdba8da72380fa1863b416b827a7
Aziz Ahmed <azizahmedsabuj@gmail.com>
2023-02-26 15:56:37 +0600

    BG-remove task

--- app/Http/Controllers/VideoMakerController.php
+++ app/Http/Controllers/VideoMakerController.php
@@ -71,7 +71,7 @@ class VideoMakerController extends Controller
 
     public static function generateIntro(Keyword $keyword)
     {
-        $image = Image::make(storage_path('app/template/title-template.jpg'));
+        $image = Image::make(storage_path('app/template/title-template.png'));
         $title = "Top Five Best $keyword->keyword";
         $image->text(strtoupper(wordwrap($title, 50)), 500, 300, function ($font) use ($title) {
             $font->file(storage_path("app/fonts/title-font.ttf"));
@@ -82,7 +82,7 @@ class VideoMakerController extends Controller
         });
 
         //generate unique name for image
-        $image_path = 'app/images/' . Str::uuid() . ".jpg";
+        $image_path = 'app/images/' . Str::uuid() . ".png";
         $image->resize(1280, 720);
         $image->save(storage_path($image_path));
 
@@ -105,7 +105,7 @@ class VideoMakerController extends Controller
 
         $video_path = "app/videos/" . Str::uuid() . ".mp4";
 
-        $ffmpegCommand = env('FFMPEG_BINARIES') . " -loop 1 -i " . storage_path($image_path) . " -i " . storage_path($audio_path) . " -c:v libx264 -tune stillimage -c:a aac -strict experimental -b:a 192k -pix_fmt yuv420p -shortest -fflags shortest -max_interleave_delta 100M " . storage_path($video_path);
+        $ffmpegCommand = env('FFMPEG_BINARIES') . " -loop 1 -i " . storage_path($image_path) . " -i " . storage_path($audio_path) . " -c:v png -c:a aac -strict experimental -b:a 192k -pix_fmt rgba -shortest -fflags shortest -max_interleave_delta 100M " . storage_path($video_path);
 
         $process = Process::fromShellCommandline($ffmpegCommand);
         $process->setTimeout(1000000);
@@ -126,7 +126,7 @@ class VideoMakerController extends Controller
 
     public static function generatePrimaryImage(Product $product)
     {
-        $primaryTemplate = Image::make(storage_path('app/template/primary_image.jpg'));
+        $primaryTemplate = Image::make(storage_path('app/template/primary_image.png'));
         $primaryImage = Image::make($product->primary_image_url);
 
         $primaryImage->resize(null, 400, function ($constraint) {
@@ -149,7 +149,7 @@ class VideoMakerController extends Controller
         $primaryTemplate->resize(1280, 720);
 
         //generate unique name for image
-        $image_path = 'app/images/' . Str::uuid() . ".jpg";
+        $image_path = 'app/images/' . Str::uuid() . ".png";
 
         $primaryTemplate->save(storage_path($image_path));
 
@@ -168,7 +168,7 @@ class VideoMakerController extends Controller
         $image_count = count($product->image_urls);
 
         foreach ($product->scripts as $index => $script) {
-            $template = Image::make(storage_path('app/template/product_image.jpg'));
+            $template = Image::make(storage_path('app/template/product_image.png'));
 
             $image = Image::make($product->image_urls[$index % $image_count]);
 
@@ -186,7 +186,7 @@ class VideoMakerController extends Controller
 
             $template->resize(1280, 720);
             //generate unique name for image
-            $image_path = 'app/images/' . Str::uuid() . ".jpg";
+            $image_path = 'app/images/' . Str::uuid() . ".png";
             $template->save(storage_path($image_path));
 
             //save attachment to database
@@ -229,7 +229,7 @@ class VideoMakerController extends Controller
         for ($i = 0; $i < count($audios); $i++) {
 
             $video_path = 'app/videos/' . Str::uuid() . ".mp4";
-            $ffmpegCommand = env('FFMPEG_BINARIES') . " -loop 1 -i " . storage_path($images[$i]->name) . " -i " . storage_path($audios[$i]->name) . " -c:v libx264 -tune stillimage -c:a aac -strict experimental -b:a 192k -pix_fmt yuv420p -shortest -fflags shortest -max_interleave_delta 100M " . storage_path($video_path);
+            $ffmpegCommand = env('FFMPEG_BINARIES') . " -loop 1 -i " . storage_path($images[$i]->name) . " -i " . storage_path($audios[$i]->name) . " -c:v png -c:a aac -strict experimental -b:a 192k -pix_fmt rgba -shortest -fflags shortest -max_interleave_delta 100M " . storage_path($video_path);
 
             Log::info($ffmpegCommand);
             $process = Process::fromShellCommandline($ffmpegCommand);
@@ -322,6 +322,7 @@ class VideoMakerController extends Controller
 
     public static function mergeBackGroundAudio(Keyword $keyword)
     {
+        $bgVideo = "app/template/bg.mp4";
         $background_audio_path = "app/template/audio.mp3";
         $video = $keyword->attachments()->where('type', 'output_video')->first();
 
@@ -333,7 +334,7 @@ class VideoMakerController extends Controller
 
         $video_path = "$folder/keyword_" . $keyword->id . $keyword->keyword . ".mp4";
 
-        $ffmpegCommand = env('FFMPEG_BINARIES') . " -i " . storage_path($video->name) . " -i " . storage_path($background_audio_path) . " -filter_complex \"[0:a]volume=1[a1];[1:a]volume=0.2[a2];[a1][a2]amix=inputs=2[a]\" -map 0:v -map \"[a]\"  -c:v copy -c:a aac -strict experimental -shortest " . escapeshellarg(storage_path($video_path));
+        $ffmpegCommand = env('FFMPEG_BINARIES') . " -i " . storage_path($video->name) . " -i " . storage_path($background_audio_path) . " -i " . storage_path($bgVideo) . " -filter_complex \"[2:v][0:v]overlay=x=0:y=0[overlay];[0:a]volume=1[a1];[1:a]volume=0.2[a2];[a1][a2]amix=inputs=2[a]\" -map \"[overlay]\" -map \"[a]\"  -c:v png -c:a aac -strict experimental -shortest " . escapeshellarg(storage_path($video_path));
 
         Log::info($ffmpegCommand);
         $process = Process::fromShellCommandline($ffmpegCommand);
@@ -384,7 +385,7 @@ class VideoMakerController extends Controller
         }
 
         //generate unique name for image
-        $image_path = "$folder/thumbnail.jpg";
+        $image_path = "$folder/thumbnail.png";
 
         $bg_layer->save(storage_path($image_path));
 
@@ -440,234 +441,10 @@ class VideoMakerController extends Controller
         }
     }
 
-    public function generateVideoOld(GenerateVideo $commandHandler)
-    {
-        $keyword = $commandHandler->argument("keyword");
-
-
-        // get an array of all the files in the specified directory
-        $files = File::glob(storage_path('app/videos/') . '*');
-
-        foreach ($files as $file) {
-            unlink($file);
-        }
-
-        $response = AmazonProduct::search('All', $keyword, 1);
-        $items = $response['SearchResult']['Items'];
-
-        $selectedItems = [];
-
-        $commandHandler->info("Total Products Found: " . count($items));
-
-        //generate title
-        self::generateTitle($keyword);
-
-        $productCount = 0;
-        foreach ($items as $item_index => $item) {
-
-            if ($productCount == 5) {
-                break;
-            }
-
-            try {
-                if (isset($item["Images"]["Variants"])) {
-
-                    // get an array of all the files in the specified directory
-                    $files = File::glob(storage_path('app/images/') . '*');
-
-                    foreach ($files as $file) {
-                        unlink($file);
-                    }
-
-                    $commandHandler->info("Image generate started for video:" . ($productCount + 1));
-
-                    //add primary image
-                    $template_path = "app/images/product_image_0.jpg";
-                    $primaryTemplate = Image::make(storage_path('app/template/primary_image.jpg'));
-                    $primaryImage = Image::make($item["Images"]["Primary"]['Large']['URL']);
-
-                    $primaryImage->resize(null, 400, function ($constraint) {
-                        $constraint->aspectRatio();
-                    });
-                    $productTitle = $item["ItemInfo"]["Title"]["DisplayValue"];
-                    $price = $item["Offers"]["Listings"][0]["Price"]["DisplayAmount"];
-
-                    $primaryTemplate->insert($primaryImage, 'top-left', 150, 30);
-                    $primaryTemplate->text(wordwrap($productTitle, 70), 100, 500, function ($font) {
-                        $font->file(storage_path("app/fonts/ArchivoBlack-Regular.ttf"));
-                        $font->size(20);
-                        $font->color('#008037');
-                    });
-                    $primaryTemplate->text($price, 775, 355, function ($font) use ($price) {
-                        $font->file(storage_path("app/fonts/ChangaOne-Regular.ttf"));
-                        $font->size(220 / strlen($price));
-                        $font->color('#00C2CB');
-                    });
-
-                    $primaryTemplate->resize(1280, 720);
-                    $primaryTemplate->save(storage_path($template_path));
-
-                    $images = $item["Images"]["Variants"];
-                    foreach ($images as $image_index => $image) {
-
-                        if ($image_index > 5) {
-                            break;
-                        }
-                        $template_path = "app/images/product_image_" . ($image_index + 1) . ".jpg";
-
-                        $template = Image::make(storage_path('app/template/product_image.jpg'));
-
-                        $image = Image::make($image['Large']['URL']);
-                        $image->resize(400, null, function ($constraint) {
-                            $constraint->aspectRatio();
-                        });
-
-                        $template->insert($image, 'left', 50, 0);
-
-
-                        $featureIndex = $image_index % count($item["ItemInfo"]["Features"]["DisplayValues"]);
-                        $feature = $item["ItemInfo"]["Features"]["DisplayValues"][$featureIndex];
-                        $template->text(wordwrap($feature, 35), 550, 120, function ($font) {
-                            $font->file(storage_path("app/fonts/ABeeZee-Regular.ttf"));
-                            $font->size(20);
-                            $font->color('#008037');
-                        });
-
-                        $template->resize(1280, 720);
-                        $template->save(storage_path($template_path));
-                    }
-
-                    $commandHandler->info("Image generate completed for video:" . ($productCount + 1));
-
-                    $commandHandler->info("Video generate started for video:" . ($productCount + 1));
-                    //generate_video
-                    $outputFile = storage_path("app/videos/video_$item_index.mp4");
-//                $ffmpegCommand = env('FFMPEG_BINARIES') . " -y -framerate 1/3 -i " . storage_path('app/images/product_image_%d.jpg') . " -r 25 -c:v libx264 -pix_fmt yuv420p " . escapeshellarg($outputFile);
-
-                    $slideDuration = 7;
-
-                    //get input images from a directory
-                    $inputImages = glob(storage_path('app/images/') . '*');
-                    $inputFilesCommand = "";
-                    foreach ($inputImages as $inputImage) {
-                        $inputFilesCommand .= " -loop 1 -t $slideDuration -i " . escapeshellarg($inputImage);
-                    }
-
-                    $ffmpegCommand = env('FFMPEG_BINARIES') . " $inputFilesCommand ";
-
-                    //add complex filter for animation
-                    $ffmpegCommand .= " -filter_complex \"";
-                    foreach ($inputImages as $inputImageIndex => $inputImage) {
-                        if ($inputImageIndex == 0) {
-                            $ffmpegCommand .= "[$inputImageIndex:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,fade=t=out:st=" . ($slideDuration - 1) . ":d=1[v$inputImageIndex];";
-                        } else {
-                            $ffmpegCommand .= "[$inputImageIndex:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1,fade=t=in:st=0:d=1,fade=t=out:st=" . ($slideDuration - 1) . ":d=1[v$inputImageIndex];";
-                        }
-                    }
-
-
-                    foreach ($inputImages as $inputImageIndex => $inputImage) {
-                        $ffmpegCommand .= "[v$inputImageIndex]";
-                    }
-
-                    $ffmpegCommand .= "concat=n=" . count($inputImages) . ":v=1:a=0,format=yuv420p[v]\" -map \"[v]\" -r 30 " . escapeshellarg($outputFile);
-
-                    $process = Process::fromShellCommandline($ffmpegCommand);
-                    $process->setTimeout(1000000);
-                    $process->run();
-
-                    if (!$process->isSuccessful()) {
-                        throw new ProcessFailedException($process);
-                    }
-
-                    echo $process->getOutput();
-
-                    $commandHandler->info("Video generate completed for video:" . ($productCount + 1));
-
-                    $selectedItems[] = $item;
-                    $productCount++;
-
-                }
-            } catch (Exception $exception) {
-                \Illuminate\Support\Facades\Log::info($exception->getMessage());
-                continue;
-            }
-        }
-
-
-        //add all video
-        // get an array of all the files in the specified directory
-        $inputFiles = glob(storage_path('app/videos/video_') . '*');
-
-        $commandHandler->info("Video merge started");
-
-        // build the command string
-        $command = env('FFMPEG_BINARIES');
-        $command .= " -i " . escapeshellarg(storage_path('app/template/intro.mp4'));
-        $command .= " -i " . escapeshellarg(storage_path('app/videos/0_title_video.mp4'));
-        for ($i = count($inputFiles) - 1; $i >= 0; $i--) {
-            $inputFile = $inputFiles[$i];
-
-            $command .= " -i " . escapeshellarg(storage_path('app/template/' . ($i + 1) . '.mp4'));
-            $command .= " -i " . escapeshellarg($inputFile);
-        }
-        $command .= " -i " . escapeshellarg(storage_path('app/template/outro.mp4'));
-//    $command .= " -i " . escapeshellarg(storage_path('app/template/audio.mp3'));
-        $outputFile = storage_path('app/output/video.mp4');
-        $command .= ' -filter_complex "concat=n=' . (count($inputFiles) + 8) . ':v=1:a=0" -vsync 2 -y ' . escapeshellarg($outputFile);
-
-
-//    return $command;
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(1000000);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-
-        echo $process->getOutput();
-
-        $commandHandler->info("Video merge completed");
-
-        $commandHandler->info("Audio merge started");
-
-        //add audio with video
-        $command = env('FFMPEG_BINARIES');
-        $command .= " -i " . escapeshellarg($outputFile);
-        $command .= " -i " . escapeshellarg(storage_path('app/template/audio.mp3'));
-        $outputFile = storage_path('app/output/video_with_audio.mp4');
-        $command .= ' -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 -shortest -y ' . escapeshellarg($outputFile);
-
-        Log::info($command);
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(1000000);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        echo $process->getOutput();
-
-        //move output file
-        $video_name = $keyword . "_video_" . time() . ".mp4";
-        File::move(storage_path('app/output/video_with_audio.mp4'), base_path('AllVideos/' . $video_name . '.mp4'));
-
-        $commandHandler->info("Audio merge completed");
-
-        //generate links
-        self::generateLinksFile($selectedItems, $keyword);
-
-        $commandHandler->info("Video generate completed");
-        $commandHandler->info("Video path: " . base_path('AllVideos/' . $video_name));
-
-    }
 
     public static function generateTitle($keyword)
     {
-        $image = Image::make(storage_path('app/template/title-template.jpg'));
+        $image = Image::make(storage_path('app/template/title-template.png'));
         $title = "Top Five Best $keyword";
         $image->text(strtoupper($title), 500, 300, function ($font) use ($title) {
             $font->file(storage_path("app/fonts/title-font.ttf"));
@@ -677,12 +454,12 @@ class VideoMakerController extends Controller
             $font->color('#FFFFFF');
         });
 
-        $image_path = storage_path('app/images/0_title_image.jpg');
+        $image_path = storage_path('app/images/0_title_image.png');
         $image->resize(1280, 720);
         $image->save($image_path);
 
 
-        $ffmpegCommand = env('FFMPEG_BINARIES') . " -y -framerate 1/5 -i $image_path -r 30 -c:v libx264 -pix_fmt yuv420p " . escapeshellarg(storage_path('app/videos/0_title_video.mp4'));
+        $ffmpegCommand = env('FFMPEG_BINARIES') . " -y -framerate 1/5 -i $image_path -r 30 -c:v png -pix_fmt rgba " . escapeshellarg(storage_path('app/videos/0_title_video.mp4'));
 
         $process = Process::fromShellCommandline($ffmpegCommand);
         $process->setTimeout(1000000);
